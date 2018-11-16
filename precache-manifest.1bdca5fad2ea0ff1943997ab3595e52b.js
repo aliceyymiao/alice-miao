133cc01c7ea63afc5567eddcba031d098184a161
Alice Miao <Wu@D-10-19-105-217.dhcp4.washington.edu>
2018-11-15 21:40:19 -0800

    Updates

--- precache-manifest.1bdca5fad2ea0ff1943997ab3595e52b.js
+++ precache-manifest.1bdca5fad2ea0ff1943997ab3595e52b.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/alice-miao/static/js/runtime~main.bf8a3744.js"
   },
   {
-    "revision": "33a17bd93b8079c3cb96",
-    "url": "/alice-miao/static/js/main.33a17bd9.chunk.js"
+    "revision": "8c558a74a9cb49c186ef",
+    "url": "/alice-miao/static/js/main.8c558a74.chunk.js"
   },
   {
     "revision": "9579cdd21063f15772f7",
     "url": "/alice-miao/static/js/1.9579cdd2.chunk.js"
   },
   {
-    "revision": "33a17bd93b8079c3cb96",
-    "url": "/alice-miao/static/css/main.b3c115cc.chunk.css"
+    "revision": "8c558a74a9cb49c186ef",
+    "url": "/alice-miao/static/css/main.1bdf1f72.chunk.css"
   },
   {
     "revision": "9579cdd21063f15772f7",
     "url": "/alice-miao/static/css/1.d11c7681.chunk.css"
   },
   {
-    "revision": "e8d751d59e9240c6af29b2ede1126dd2",
+    "revision": "01e951a4b64acb12b1698a92292efab1",
     "url": "/alice-miao/index.html"
   }
 ];
