cb99b16baef2781d7b47c2d92e5af332191a5b95
Alice Miao <Wu@D-10-19-105-217.dhcp4.washington.edu>
2018-11-15 21:04:47 -0800

    publish

---
+++ build/precache-manifest.c0402d86192505ecfa345af70cbc979d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf8a374453542efd2baa",
+    "url": "/alice-miao/static/js/runtime~main.bf8a3744.js"
+  },
+  {
+    "revision": "33a17bd93b8079c3cb96",
+    "url": "/alice-miao/static/js/main.33a17bd9.chunk.js"
+  },
+  {
+    "revision": "9579cdd21063f15772f7",
+    "url": "/alice-miao/static/js/1.9579cdd2.chunk.js"
+  },
+  {
+    "revision": "33a17bd93b8079c3cb96",
+    "url": "/alice-miao/static/css/main.b3c115cc.chunk.css"
+  },
+  {
+    "revision": "9579cdd21063f15772f7",
+    "url": "/alice-miao/static/css/1.d11c7681.chunk.css"
+  },
+  {
+    "revision": "e8d751d59e9240c6af29b2ede1126dd2",
+    "url": "/alice-miao/index.html"
+  }
+];
---
+++ src/About.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+export class About extends Component {
+    render() {
+        return <div className="container">I am an international student from China. This is my third year in Seattle and I just got my Washington State driver's liscense. And then I became really into cars. I love looking at different kinds of cars and hope one day I can buy one of them listed in my interests page.</div>
+    }
+}
